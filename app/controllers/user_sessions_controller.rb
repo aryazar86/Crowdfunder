0e1479780cd3f6d3c0a20944b53d0c4cc4167514
MatthewTLackey <lackey.matt@gmail.com>
2013-12-05 19:49:09 -0500

    can delete a project

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -5,7 +5,7 @@ class UserSessionsController < ApplicationController
   end
 
   def create
-    user = login(params[:email], params[:password], params[:remember_me_token])
+    user = login(params[:email], params[:password], params[:remember_me])
 
     if user
       redirect_back_or_to(:projects, notice: 'Login successful')
